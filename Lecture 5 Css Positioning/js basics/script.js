fca1faf819d716e54c853a4a5a3e0597e8a53a5e
Vasu Gupta <vasugupta.com@gmail.com>
2020-12-08 20:58:08 +0530

    js basics added

---
+++ Lecture 5 Css Positioning/js basics/script.js
@@ -0,0 +1,13 @@
+function a(){
+    return 1
+}
+
+console.log(typeof 112)
+console.log(typeof 101.255)
+console.log(typeof 'a')
+console.log(typeof 'aldjfal')
+console.log(typeof true)
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(a,typeof a)
+console.log(a(),typeof a())
