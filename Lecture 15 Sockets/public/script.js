38d465bbd821243760c95fb8f13b50f8a5eb3374
Vasu Gupta <vasugupta.com@gmail.com>
2020-12-26 18:59:24 +0530

    final code added

--- Lecture 15 Sockets/public/script.js
+++ Lecture 15 Sockets/public/script.js
@@ -1,14 +1,34 @@
 let socket = io()
 
+$('#loginBox').show()
+$('#chatbox').hide()
 
-let boomBtn = document.getElementById('boom')
+$('#btnLogin').click(()=>{
+    socket.emit('login',{
+        username: $('#inpUsername').val(),
+        password: $('#inpPass').val()
+    })
 
-boomBtn.onclick = function(){
-    socket.emit('boom')
-}
+})
 
-socket.on("whizz",()=>{
-    let div = document.createElement('div')
-    div.innerText = "whizz from server"
-  document.body.appendChild(div)
+socket.on("logged_in",()=>{
+    $('#loginBox').hide()
+    $('#chatbox').show()
+})
+
+$('#btnSend').click(()=>{
+    socket.emit('msg_send',{
+        to: $('#inpToUser').val(),
+        msg: $('#inpNewMsg').val()
+    })
+})
+
+socket.on("msg_rcvd",(data)=>{
+    $('#ulMsglist').append($('<li>').text(
+        `[${data.from}] : ${data.msg}`
+    ))
+})
+
+socket.on('login_failed',()=>{
+    window.alert("Password is wrong")
 })
--- Lecture 15 Sockets/server.js
+++ Lecture 15 Sockets/server.js
@@ -2,22 +2,62 @@ const http = require("http")
 const express = require('express')
 const app = express()
 const sockeIo = require('socket.io')
+const { S_IFDIR } = require("constants")
 
 const server = http.createServer(app)
 const io = sockeIo(server)
 
+let users = {
+    'vasu': 'vasu123',
+    'tanish': 'tanish123'
+}
+
+let socketmap = {
+
+}
+
+function login(s,u){
+    s.join(u)
+    s.emit("logged_in")
+    socketmap[s.id] = u
+    console.log(socketmap)
+}
+
+
 io.on('connection',(socket)=>{ 
     console.log("Connected with socket id: ",socket.id)
 
-    socket.on('boom',()=>{
-        console.log("Boom received from ",socket.id)
+    socket.on('login',(data)=>{
+        if(users[data.username]){
+            if(users[data.username]==data.password){
+                login(socket,data.username)
+
+            }else{
+                socket.emit("login_failed")
+            }
+            
+
+        }else{
+            users[data.username] = data.password
+            login(socket,data.username)
+        }
+
+        console.log(users)
     })
 
-    setInterval(()=>{
-        socket.emit("whizz")
-    },2000)
+    socket.on("msg_send",(data)=>{
+        data.from = socketmap[socket.id]
+        if(data.to){
+            io.to(data.to).emit('msg_rcvd',data)
+        }else{
+            socket.broadcast.emit('msg_rcvd',data)
+        }
+    })
 
 })
+
+
+
 app.use('/',express.static(__dirname+'/public'))
 
 
